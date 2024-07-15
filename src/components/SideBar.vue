ac41319c2679dfa20c9bf4d87ba36dc5ea134055
Daniel Hibbin <dhibbin2@gmail.com>
2024-07-15 14:53:57 +0100

    Rule and setting definition now in same object

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -4,37 +4,37 @@
       <v-expansion-panel-title>Settings</v-expansion-panel-title>
       <v-expansion-panel-text class="pa-0">
         <v-text-field
-          v-model="settings.ipAddress"
+          v-model="settings.ipAddress.value"
           label="IP Address"
-          :rules="LLMSettingsWrapper.rules.ipAddress.value"
+          :rules="LLMSettingsWrapper.rules.ipAddress.rule.value"
           class="rounded pa-1 ma-0"
         />
         <v-text-field
-          v-model="settings.n_predict"
+          v-model="settings.n_predict.value"
           class="rounded pa-1 ma-0"
           type="number"
           label="n_predict"
           persistent-hint
-          :rules="LLMSettingsWrapper.rules.n_predict.value"
+          :rules="LLMSettingsWrapper.rules.n_predict.rule.value"
           hint="Number of tokens to predict per generation"
         />
         <v-text-field
-          v-model="settings.n_probs"
+          v-model="settings.n_probs.value"
           class="rounded pa-1 ma-0"
           type="number"
           label="n_probs"
           persistent-hint
           hint="Maximum number of alternative tokens per token"
-          :rules="LLMSettingsWrapper.rules.n_probs.value"
+          :rules="LLMSettingsWrapper.rules.n_probs.rule.value"
         />
         <v-text-field
-          v-model.number="settings.seed"
+          v-model.number="settings.seed.value"
           class="rounded pa-1 ma-0"
           type="number"
           label="Seed"
           persistent-hint
           hint="RNG seed for LLM, -1 is a random seed"
-          :rules="LLMSettingsWrapper.rules.seed.value"
+          :rules="LLMSettingsWrapper.rules.seed.rule.value"
         />
       </v-expansion-panel-text>
     </v-expansion-panel>
@@ -46,12 +46,7 @@
   import { ref, type Ref, reactive, watch } from 'vue'
   import { LLMService, type LLMSettings, LLMSettingsWrapper } from '@/objects/LLMService';
 
-  const settings : LLMSettings = reactive({
-    n_predict : 10,
-    n_probs : 5,
-    seed : -1,
-    ipAddress : "localhost:8080"
-  })
+  const settings : LLMSettings = LLMSettingsWrapper.rules
 
   const panel : Ref<number[]> = ref([0])
 
--- src/objects/LLMService.ts
+++ src/objects/LLMService.ts
@@ -1,14 +1,8 @@
-import { computed, type ComputedRef } from 'vue'
-import { ru } from 'vuetify/locale'
+import { computed, type ComputedRef, reactive } from 'vue'
 
 export class LLMService {
     private static wrappedInstance : LLMService
-    private wrappedSettings : LLMSettings = {
-        n_predict : 10,
-        n_probs : 5,
-        seed : -1,
-        ipAddress : "localhost:8080"
-    }
+    private wrappedSettings : LLMSettings = LLMSettingsWrapper.rules
 
     private constructor() {
 
@@ -28,14 +22,6 @@ export class LLMService {
     } 
 }
 
-export interface LLMSettings {
-    [key: string]: number | string; 
-    n_predict : number
-    n_probs : number
-    seed : number
-    ipAddress : string
-}
-
 type RuleType = ComputedRef<((v: string) => boolean | string)[]> | ComputedRef<((v: number) => boolean | string)[]>;
 
 class SettingsValue {
@@ -48,64 +34,54 @@ class SettingsValue {
     }
 }
 
-export class SettingsList {
-    public n_predict : SettingsValue = new SettingsValue(
-        0,
-        computed(() => [
-            (v: number) : boolean | string => v >= 0 || 'n_predict must be non-negative',
-            // Add more rules as needed
-        ])
-    )
+export interface LLMSettings {
+    [key: string]: SettingsValue; 
+    n_predict : SettingsValue,
+    n_probs : SettingsValue,
+    seed : SettingsValue,
+    ipAddress : SettingsValue
 }
-
-
-interface SettingsRules {
-    [key : string] : RuleType;
-    n_predict : RuleType,
-    n_probs : RuleType,
-    seed : RuleType,
-    ipAddress : RuleType
-}
-
 
 
 export class LLMSettingsWrapper {
-    public static readonly rules : SettingsRules = {
-        n_predict : computed(() => [
+    public static readonly rules : LLMSettings = reactive({
+        n_predict : new SettingsValue(
+            0, 
+            computed(() => [
             (v: number) : boolean | string => v >= 0 || 'n_predict must be non-negative',
             // Add more rules as needed
-        ]),
-        n_probs : computed(() => [
-            (v: number) : boolean | string => v >= 0 || 'n_probs must be non-negative',
-            // Add more rules as needed
-        ]),
-        seed : computed(() => [
-            (v: number) : boolean | string => Number.isInteger(v) || 'seed must be an integer',
-            // Add more rules as needed
-        ]),
-        ipAddress : computed(() => [
-            (v: string) : boolean | string => !!v || 'IP address is required',
-            (v: string) : boolean | string => LLMSettingsWrapper.validateIPAddress(v) || 'Invalid IP address',
-        ]),
-    }
+        ])),
+        n_probs : new SettingsValue(
+            0,
+            computed(() => [
+                (v: number) : boolean | string => v >= 0 || 'n_probs must be non-negative',
+                // Add more rules as needed
+            ])
+        ),
+        seed : new SettingsValue(
+            -1,
+            computed(() => [
+                (v: number) : boolean | string => Number.isInteger(v) || 'seed must be an integer',
+                // Add more rules as needed
+            ])
+        ),
+        ipAddress : new SettingsValue(
+            "localhost:0000",
+            computed(() => [
+                (v: string) : boolean | string => !!v || 'IP address is required',
+                (v: string) : boolean | string => LLMSettingsWrapper.validateIPAddress(v) || 'Invalid IP address',
+            ])
+        )
+    })
 
     public static validate(settings : LLMSettings) : boolean {
         let isValid = true
-        
-        for (const key in LLMSettingsWrapper.rules) {
-            const typedKey = key as keyof LLMSettings;
-            const currentRules = LLMSettingsWrapper.rules[typedKey].value
-            const value = settings[typedKey]
-            currentRules.forEach(element => {
-                if (element(value) !== true) { // This argument do
-                    isValid = false
-                    console.log(typeof value)
-                    console.log(isValid)
-                    return isValid
-                }
-            });
+
+        for (const key in settings) {
+            for (const rule in settings[key].rule) {
+                console.log(settings[key].value)
+            }
         }
-        
 
         return isValid
     }
