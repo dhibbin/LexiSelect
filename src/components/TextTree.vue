279d3fe5008eea225dd2624545df4acdcfa62380
Daniel Hibbin <dhibbin2@gmail.com>
2024-07-18 19:09:37 +0100

    Cannot locate cursor position still

--- src/components/TextTree.vue
+++ src/components/TextTree.vue
@@ -1,29 +1,33 @@
 <template>
   <div style="position: relative; width: 100%; height: 200px; overflow-y: scroll; overflow-x: scroll;">
     <input
+      ref="inputText"
       v-model="localText"
       type="text"
       style="position: absolute; top: 0; left: 0; overflow-y: hidden; font-family: monospace;"
       :style="{ width: localText.length + 'ch' }"
       rows="1"
+      @keydown="locateCursor"
     >
 
     <span
-      v-for="(word, index) in words"
+      v-for="(token, index) in tokens"
       :key="index"
       style="position: absolute; top: 1px; left: 0; font-family: monospace;"
-      :style="{ left : characterLengths[index] + 'ch'}"
+      :style="{ left : tokens[index].charOffset + 'ch'}"
       class="spanText"
       @mouseover="hover(true, index)"
-    >{{ word }}</span>
+    >{{ stripSpaces(token.completionProb.content) }}</span>
 
     <v-expand-transition>
       <v-card
-        v-show="expand"
+        v-show="expand && !tokens[expandPanelIndex].userModified"
         style="overflow: visible;"
-        :style="{ position: 'absolute', top: '20px', left: characterLengths[expandPanelIndex] + 'ch', fontFamily: 'monospace' }"
+        :style="{ position: 'absolute', top: '20px', 
+                  left: (tokens.length > 0 ? tokens[expandPanelIndex].charOffset : '0')
+                    + 'ch', fontFamily: 'monospace' }"
         class="mx-auto bg-secondary"
-        
+        width="200px"
         @mouseover="expand = true"
         @mouseleave="expand = false"
       >
@@ -35,15 +39,27 @@
 
 <script setup lang="ts">
   import type { Completionprobability, LlamaInterface } from '@/objects/LlamaInterface';
-  import { computed, defineProps, ref, watch, type Ref } from 'vue'
+  import { computed, defineProps, reactive, ref, watch, type Ref, type ComputedRef } from 'vue'
 
   interface TreeToken {
     completionProb : Completionprobability
-    spanContent : string
-    characterOffset : number
+    charOffset : number
+    userModified : boolean
   }
 
-  const currentTokens : Ref<TreeToken[]> = ref([])
+  const tokens : ComputedRef<TreeToken[]> = computed(() => {
+    let newTokens : TreeToken[] = []
+    let previousCharacters = 0
+    for (let i = 0; i < props.responseLLM.completion_probabilities.length; i++) {
+      newTokens.push( reactive({
+        completionProb : props.responseLLM.completion_probabilities[i],
+        charOffset : previousCharacters,
+        userModified : false
+      }))
+      previousCharacters += props.responseLLM.completion_probabilities[i].content.length
+    }
+    return newTokens
+  })
 
   const emits = defineEmits<{
       textUpdate : [newText : string]
@@ -54,41 +70,47 @@
   }>()
 
   const localText = ref(props.responseLLM.content)
-  const expandPanelIndex : Ref<number> = ref(-1)
+  const expandPanelIndex : Ref<number> = ref(0)
   const expand = ref(false)
-
-  const words = computed(() => {
-    let splitWords = localText.value.split(" ")
-    return splitWords.map((str : string, index : number) => index !== 0 ? "_" + str : str);
-  })
-
-  const characterLengths = computed(() => {
-    let previousCharacters = 0
-    let prevCharList : number[] = []
-    for (let i = 0; i < words.value.length; i++) {
-      prevCharList.push(previousCharacters)
-      previousCharacters += words.value[i].length
-    }
-    return prevCharList
-  })
+  const inputText = ref()
 
   watch(props, () => {
     localText.value = props.responseLLM.content
-    for (const token in props.responseLLM.completion_probabilities) {
-      console.log(token)
-    }
-    
-    console.log(JSON.stringify(props.responseLLM))
+    //previousLocalText = props.responseLLM.content
   })
 
   watch(localText, () => {
     emits("textUpdate", localText.value)
+    let foundChange = false
+    for (let i = 0; i < tokens.value.length; i++) {
+      for (let n = 0; n < tokens.value[i].completionProb.content.length; n++) {
+        let globalIndex = tokens.value[i].charOffset
+        //console.log(localText.value[globalIndex], tokens.value[i].completionProb.content[n])
+        if (localText.value[globalIndex + n] != tokens.value[i].completionProb.content[n] && !foundChange) {
+          console.log("found changed char", globalIndex, localText.value[n], tokens.value[i].completionProb.content[n])
+          console.log(tokens.value[i].completionProb.content)
+          foundChange = true
+        }
+      }
+      
+    }
   })
 
   function hover(openHover : boolean, tokenIndex : number) : void {
     expandPanelIndex.value = tokenIndex
     expand.value = openHover
   }
+
+  function stripSpaces(oldString : string) : string {
+    return oldString.replace(/ /g, '_')
+  }
+
+  function locateCursor(event : Event) : void {
+    event.preventDefault()
+    let textBody = inputText
+    console.log(event.clientX)
+  }
+
 
 </script>
 
