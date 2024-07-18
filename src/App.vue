74fa7c41bef72e27bb5ef528fe2014986f2893d7
Daniel Hibbin <dhibbin2@gmail.com>
2024-07-18 16:58:36 +0100

    Added testvalue button

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-app-bar-title>Lexi Select</v-app-bar-title>
+      <v-btn
+        icon="mdi-gavel" 
+        variant="text" 
+        @click="sendTestJson"
+      />
     </v-app-bar>
 
     <v-navigation-drawer
@@ -21,7 +26,10 @@
       class="d-flex align-center justify-center"
       style="overflow-y: hidden;"
     >
-      <TextTree :generated-text="paragraph" />
+      <TextTree
+        :generated-text="paragraph"
+        :response-l-l-m="latestResponse"
+      />
     </v-main>
   </v-app>
 </template>
@@ -31,13 +39,20 @@
   import SideBar from './components/SideBar.vue'
   import TextBar from './components/TextBar.vue'
   import TextTree from './components/TextTree.vue'
-  import type { LlamaInterface } from './objects/LlamaInterface';
+  import testResponse from './assets/testResponse.json'
+  import { defaultLlamaInterface, type LlamaInterface } from './objects/LlamaInterface';
 
   const drawer : Ref<boolean> = ref(true)
-  const paragraph : Ref<string> = ref("Hello World")
+  const paragraph : Ref<string> = ref("")
+  const latestResponse : Ref<LlamaInterface> = ref(defaultLlamaInterface())
 
   function onGenerationRecieved(value : LlamaInterface) : void {
     paragraph.value = value.content
+    latestResponse.value = value
+  }
+
+  function sendTestJson() : void {
+    latestResponse.value = JSON.parse(JSON.stringify(testResponse))
   }
 
 
--- src/components/TextTree.vue
+++ src/components/TextTree.vue
@@ -15,71 +15,80 @@
       :style="{ left : characterLengths[index] + 'ch'}"
       class="spanText"
       @mouseover="hover(true, index)"
-
     >{{ word }}</span>
 
     <v-expand-transition>
       <v-card
         v-show="expand"
-        :style="{ position: 'absolute', top: '20px', left: characterLengths[expandPanelIndex] + 'ch' }"
+        style="overflow: visible;"
+        :style="{ position: 'absolute', top: '20px', left: characterLengths[expandPanelIndex] + 'ch', fontFamily: 'monospace' }"
         class="mx-auto bg-secondary"
-        height="100"
-        width="100"
+        
         @mouseover="expand = true"
         @mouseleave="expand = false"
-      />
+      >
+        <v-list :items="[{title:'hellotitle', value : 1}]" />
+      </v-card>
     </v-expand-transition>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, defineProps, ref, watch, type Ref } from 'vue'
+  import type { Completionprobability, LlamaInterface } from '@/objects/LlamaInterface';
+  import { computed, defineProps, ref, watch, type Ref } from 'vue'
 
-const emits = defineEmits<{
-    textUpdate : [newText : string]
-}>()
-
-const props = defineProps<{
-    generatedText : string
-}>()
-
-const localText = ref(props.generatedText)
-const expandPanelIndex : Ref<number> = ref(-1)
-const expand = ref(false)
-
-const words = computed(() => {
-  let splitWords = localText.value.split(" ")
-  return splitWords.map((str : string, index : number) => index !== 0 ? "_" + str : str);
-})
-
-const expandPanels = computed(() => {
-  return new Array(words.value.length).fill(false)
-})
-
-const characterLengths = computed(() => {
-  let previousCharacters = 0
-  let prevCharList : number[] = []
-  for (let i = 0; i < words.value.length; i++) {
-    prevCharList.push(previousCharacters)
-    previousCharacters += words.value[i].length
+  interface TreeToken {
+    completionProb : Completionprobability
+    spanContent : string
+    characterOffset : number
   }
-  return prevCharList
-})
 
-watch(props, () => {
-  localText.value = props.generatedText
-})
+  const currentTokens : Ref<TreeToken[]> = ref([])
 
-watch(localText, () => {
-  emits("textUpdate", localText.value)
-})
+  const emits = defineEmits<{
+      textUpdate : [newText : string]
+  }>()
 
-function hover(openHover : boolean, tokenIndex : number) : void {
-  expandPanelIndex.value = tokenIndex
-  expandPanels.value[tokenIndex] = openHover
-  console.log(expandPanels.value[expandPanelIndex.value])
-  expand.value = openHover
-}
+  const props = defineProps<{
+      responseLLM : LlamaInterface
+  }>()
+
+  const localText = ref(props.responseLLM.content)
+  const expandPanelIndex : Ref<number> = ref(-1)
+  const expand = ref(false)
+
+  const words = computed(() => {
+    let splitWords = localText.value.split(" ")
+    return splitWords.map((str : string, index : number) => index !== 0 ? "_" + str : str);
+  })
+
+  const characterLengths = computed(() => {
+    let previousCharacters = 0
+    let prevCharList : number[] = []
+    for (let i = 0; i < words.value.length; i++) {
+      prevCharList.push(previousCharacters)
+      previousCharacters += words.value[i].length
+    }
+    return prevCharList
+  })
+
+  watch(props, () => {
+    localText.value = props.responseLLM.content
+    for (const token in props.responseLLM.completion_probabilities) {
+      console.log(token)
+    }
+    
+    console.log(JSON.stringify(props.responseLLM))
+  })
+
+  watch(localText, () => {
+    emits("textUpdate", localText.value)
+  })
+
+  function hover(openHover : boolean, tokenIndex : number) : void {
+    expandPanelIndex.value = tokenIndex
+    expand.value = openHover
+  }
 
 </script>
 
